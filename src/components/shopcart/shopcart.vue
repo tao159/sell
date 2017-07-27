84be301586809e700bfab32e0cc3d08abeb4234b
yt159 <yangtao155@163.com>
2017-07-27 18:11:28 +0800

    项目初始化，goods开发

---
+++ src/components/shopcart/shopcart.vue
@@ -0,0 +1,273 @@
+<template>
+    <div class="shopcart">
+        <div class="content" @click="toggleList">
+            <div class="content-left">
+                <div class="logo-wrapper">
+                    <div class="logo" :class='{"highlight":totalCount>0}'>
+                        <i class="icon-shopping_cart" :class='{"highlight":totalCount>0}'></i>
+                    </div>
+                    <div class="num" v-show="totalCount>0">{{totalCount}}</div>
+                </div>
+                <div class="price" :class='{"highlight":totalPrice>0}'>￥{{totalPrice}}</div>
+                <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
+            </div>
+            <div class="content-right">
+                <div class="pay" :class="payClass">
+                    {{payDesc}}
+                </div>
+            </div>
+            <!-- <div class="ball-cnontainer">
+                <div transition="drop" class="ball" v-for="ball in balls" v-show="ball.show">
+                    <div class="inner inner-hook"></div>
+                </div>
+            </div> -->
+            <div class="shopcart-list" v-show='listShow' transition="fold">
+                <div class="list-header">
+                    <h1 class="title">购物车</h1>
+                    <span class="empty">清空</span>
+                </div>
+                <div class="list-content">
+                    <ul>
+                        <li class="food" v-for="food in selectFoods">
+                            <span class="name">{{food.name}}</span>
+                            <div class="price">
+                                <span>￥{{food.price*food.count}}</span>
+                            </div>
+                            <div class="cartcontrol-wrapper">
+                                <cartcontrol :food="food"></cartcontrol>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    import cartcontrol from 'components/cartcontrol/cartcontrol';
+    export default{
+        data(){
+            return{
+                balls:[ 
+                    {
+                        show:false
+                    },
+                    {
+                        show:false
+                    },
+                    {
+                        show:false
+                    },
+                    {
+                        show:false
+                    },
+                    {
+                        show:false
+                    }
+                ],
+                dropBalls:[],
+                flod:true
+            }
+        },
+        props:{
+            selectFoods:{
+                type:Array,
+                default(){
+                    return [];
+                }
+            },
+            deliveryPrice:{
+                type:Number,
+                dafault:0
+            },
+            minPrice:{
+                type:Number,
+                default:0
+            }
+        },
+        computed:{
+            totalPrice(){
+                let total=0;
+                this.selectFoods.forEach((food)=>{
+                    total+=food.price*food.count
+                });
+                return total;
+            },
+            totalCount(){
+                let count=0;
+                this.selectFoods.forEach((food)=>{
+                    count+=food.count;
+                })
+                return count
+            },
+            payDesc(){
+                if(this.totalPrice ===0){
+                    return `￥${this.minPrice}元起送`
+                }else if(this.totalPrice<this.minPrice){
+                    let diff=this.minPrice-this.totalPrice;
+                    return `还差￥${diff}元起送`
+                }else{
+                    return '去结算'
+                }
+            },
+            payClass(){
+                if(this.totalPrice<this.minPrice){
+                    return 'not-enough';
+                }else{
+                    return 'enough';
+                }
+
+            },
+            listShow(){
+                if(!this.totalCount){
+                    this.fold=true
+                    return false;
+                }
+                let show=!this.fold;
+                return show;
+            }
+        },
+        methods:{
+            drop(el){
+                for(let i=0;i<this.balls.length;i++){
+                    let ball=this.balls[i];
+                    if(!ball.show){ 
+                        ball.show=true;
+                        ball.el=el;
+                        this.dropBalls.push(ball);
+                        return;
+                    }
+                }
+            } ,  
+            toggleList(){
+                if(this.totalCount){
+                    return;
+                }
+                this.fold=!this.fold;  
+            }
+        },
+        components:{
+            cartcontrol
+        }
+    }
+</script>
+
+<style lang='stylus' rel="stylesheet/stylus">
+    .shopcart
+        position:fixed
+        left:0
+        bottom:0
+        width:100%
+        height:48px 
+        z-index:50
+        .content
+            display:flex
+            background:#141d27
+            font-size:0
+            .content-left
+                flex:1
+                .logo-wrapper
+                    display:inline-block
+                    position:relative
+                    top:-10px
+                    margin:0 12px
+                    padding:6px
+                    width:56px
+                    height:56px
+                    box-sizing:border-box
+                    vertical-align:top
+                    border-radius:50%
+                    background:#141d27
+                    .logo
+                        width:100%
+                        height:100%
+                        border-radius:50%
+                        background:#2b343c
+                        text-align:center
+                        &.highlight
+                            background:rgb(0,160,220)
+                        .icon-shopping_cart
+                            font-size:24px
+                            line-height:44px
+                            color:#80868a                          
+                            &.highlight
+                                color:#fff    
+                    .num
+                        position:absolute
+                        top:0
+                        right:0
+                        width:24px
+                        height:16px
+                        line-height:16px
+                        text-align:center
+                        border-radius:16px
+                        font-size:9px
+                        font-weight:700
+                        color:#fff
+                        background:red
+                        box-shadow:0 4px 8px 0 rgba(0,0,0,0.4)
+                .price
+                    display:inline-block
+                    vertical-align:top
+                    line-height:24px
+                    margin-top:12px
+                    box-sizing:border-box
+                    padding-right:12px
+                    border-right:1px solid rgba(255,255,255,0.1)
+                    font-size:16px
+                    font-weight:700
+                    color:rgba(255,255,255,0.4)
+                    &.highlight
+                        color:#fff
+                .desc
+                    display:inline-block
+                    vertical-align:top
+                    line-height:24px
+                    margin:12px 0 0 12px
+                    color:rgba(255,255,255,0.4)
+                    font-size:10px
+                    font-weight:300
+            .content-right
+                flex:0 0 105px
+                width:105px
+                .pay
+                    height:48px
+                    line-height:48px
+                    text-align:center
+                    font-size:12px
+                    color:rgba(255,255,255,0.4)
+                    font-weight:700
+                    background:#2b333b
+                    &.not-enough
+                        background:#2b333b
+                    &.enough
+                        background:#00b43c
+                        color:#fff
+
+        .ball-cnontainer
+            .ball
+                position:fixed
+                left:32px
+                bottom:22px
+                z-index:200
+                &.drop-transition
+                    transition:all 0.4s cubic-bezier(0.49,-0.29,0.75,0.41)
+                    .inner 
+                        width:16px
+                        height:16px
+                        border-radius:50%
+                        background:rgb(0,160,220)
+                        transition:all 0.4s linear 
+        .shopcart-list
+            position:absolute
+            left:0
+            top:0
+            z-index:-1
+            width:100%
+            &.fold-transition
+                transition:all 0.4s
+                transform:translate3d(0,-100%,0)
+                &.fold-enter,&fold-leave
+                    transform:translate3d(0,0,0)
+</style>
